f7a270b1753890f6c70856cb035383a3f70b2e8c
Alexander Skidan <ale@creavi.dk>
2023-04-20 10:24:59 +0300

    Set default user data to unknown;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,62 @@
 export const environment = {
-  production: true
+  production: true,
+
+  defaultUser: {
+    gender: "unknown",
+    name: {
+      title: "unknown",
+      first: "unknown",
+      last: "unknown",
+    },
+    location: {
+      street: {
+        number: 0,
+        name: "unknown",
+      },
+      city: "unknown",
+      state: "unknown",
+      country: "unknown",
+      postcode: "unknown",
+      coordinates: {
+        latitude: "unknown",
+        longitude: "unknown",
+      },
+      timezone: {
+        offset: "unknown",
+        description: "unknown",
+      },
+    },
+    email: "unknown",
+    login: {
+      uuid: "unknown",
+      username: "unknown",
+      password: "unknown",
+      salt: "unknown",
+      md5: "unknown",
+      sha1: "unknown",
+      sha256: "unknown",
+    },
+    dob: {
+      date: "unknown",
+      age: 0,
+    },
+    registered: {
+      date: "unknown",
+      age: 0,
+    },
+    phone: "unknown",
+    cell: "unknown",
+    id: {
+      name: "unknown",
+      value: "unknown",
+    },
+    picture: {
+      large: "unknown",
+      medium: "unknown",
+      thumbnail: "unknown",
+    },
+    nat: "unknown",
+  }
+
+
 };
